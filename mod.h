27ba3291d654a336401c107b1abfd9025e802c11
lintkey <lintkey@outlook.com>
2022-08-17 23:56:59 +0800

    完善注释

--- mod.h
+++ mod.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "base.h"
 
-template<class T, class U, con U &M>
+template<class T, con T &M, class U=ul>
 struct DMT {
     T raw;
     inl cer DMT(con T _raw=T()) : raw(_raw) {}
@@ -20,14 +20,22 @@ struct DMT {
 };
 
 // V3.0 M可变更模数
+// WARN: 出于效率考虑，初始化时并未对初始值取模，请手动取模 v.add(0);
+// WARN: 为了防止误用，未实现输入重载，但是可以直接输出
+// TIP: 如欲加速，请尽量使用同类型对象作为运算对象(MT += MT)，而不是(MT += T)
+// TIP: 另外，对于用不到的成员函数，也可删除以加速
+template<con us &M>
+using dms = DMT<us, M>;
+template<con ul &M>
+using dml = DMT<ul, M, ix>;
+
+// V1.0 静态M模数
 // WARN: 出于效率考虑，初始化时并未对初始值取模，请手动取模
 // WARN: 为了防止误用，未实现输入重载，但是可以直接输出
 // TIP: 如欲加速，请尽量使用同类型对象作为运算对象(MT += MT)，而不是(MT += T)
 // TIP: 另外，对于用不到的成员函数，也可删除以加速
-template<con ul &M>
-using dmt = DMT<us, ul, M>;
-
-template<class T, class U, U M>
+// U: 乘法提升时要用，避免乘法溢出, T=us, U=ul; T=ul, U=ix
+template<class T, T M, class U=ul>
 struct MT {
     T raw;
     inl cer MT(con T _raw=T()) : raw(_raw) {}
@@ -45,9 +53,7 @@ struct MT {
     friend inl MT operator*(MT l, con MT &r) { ret l.mul(r), l; }
 };
 
-// V1.0 静态M模数
-// WARN: 为了防止误用，未实现输入重载，但是可以直接输出
-// TIP: 如欲加速，请尽量使用同类型对象作为运算对象(MT += MT)，而不是(MT += T)
-// TIP: 另外，对于用不到的成员函数，也可删除以加速
-template<class U=ul, U M=MOD>
-using mt = MT<us, U, M>;
+template<us M>
+using ms = MT<us, M>;
+template<ul M>
+using ml = MT<ul, M, ix>;
