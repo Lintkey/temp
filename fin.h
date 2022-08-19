6b7051747de5038a20218c48da36832d8edfbb24
lintkey <lintkey@outlook.com>
2022-08-19 13:45:55 +0800

    默认开启缓冲 添加单个字符获取(fin(char)，当前字符获取(gr())和EOF判断(fit())

--- fin.h
+++ fin.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "base.h"
 
-// 取消注释并删除下一行可开启缓冲(针对大文件)
-#define gc() getchar()
-// cer us MAXB = 1 << 20;
-// char fbuf[MAXB], *fr = fbuf, *ed = fbuf;
-// #define fread() (ed=(fr=fbuf-1)+fread(fbuf,1,MAXB,stdin),fr==ed)
-// inl is gc() { ret(fr == ed && fread()) ? EOF : *++fr; }
-// #undef fread
+cer us MAXB = 1 << 20;
+char fbuf[MAXB], *fr = fbuf, *ed = fbuf;
+#define fread() (ed=(fr=fbuf)+fread(fbuf,1,MAXB,stdin),fr==ed)
+inl is gc() { ret (fr==ed && fread()) ? EOF : *(fr++); }
+inl is gr() { ret (fr==ed && fread()) ? EOF : *fr; }
+inl is fit() { whi(isspace(gr())) ++fr; ret gr(); }
+#undef fread
+
 template<class T>
 inl T &fin(T &v) {
-    is c=gc(); bool f=v=0;
-    whi(!isdigit(c)) f = (c=gc()) == '-';
-    for(; isdigit(c); c=gc()) v = v * 10 + c - '0';
-    ret f ? v=-v : v;
+    is c = v = 0; bool f = fit()=='-'; fr += f;
+    for(; isdigit(c=gc());) v = v*10 + c - '0';
+    ret f ? (v=-v) : v;
 }
+
+template<>
+inl char &fin(char &v) { ret fit(), v = gc(); }
