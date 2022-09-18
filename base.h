48ba17f32a843857c38f783ee46dec3d2196f037
lintkey <lintkey@outlook.com>
2022-09-18 19:22:12 +0800

    [增加]添加gosper's hack

--- base.h
+++ base.h
@@ -9,15 +9,16 @@
 #define con const
 #define cer constexpr
 #define ret return
+#define let auto
 #define whi(...) while(__VA_ARGS__)
 using namespace std;
 using is = int;
 using il = long long;
-#define ix __int128
+using ix = __int128;
 using uc = unsigned char;
 using us = unsigned int;
 using ul = unsigned long long;
-#define ux unsigned __int128
+using ux = unsigned __int128;
 using fs = float;
 using fl = double;
 using fx = long double;
@@ -26,6 +27,8 @@ template<class T> inl cer T INF() { ret T(0x3f3f3f3f3f3f3f3f); }
 cer con char *ANS[] = {"F", "T"};
 cer us MAXB = 1 << 20;
 
+#define ctz(x) __builtin_ctz(x)
+#define popcount(x) __builtin_popcount(x)
 template<class T> inl void mem(T *arr, us b, us n) { memset(arr, b, n * sf(T)); }
 template<class T> inl void tmx(T &lhs, con T &rhs) { if(rhs>lhs) lhs = rhs; }
 template<class T> inl void tmn(T &lhs, con T &rhs) { if(rhs<lhs) lhs = rhs; }
---
+++ math/gosper.h
@@ -0,0 +1,28 @@
+#include "../base.h"
+
+// C_n^k 获取n选k状压状态(从小到大)
+template<class T=us>
+vector<T> gosper(con uc n, con uc k) {
+    assert(n&&n>=k&&n<=(sf(T)<<3)); // 防止超限
+    vector<T> res;
+    T cur = (T(1)<<k) - 1;
+    T lim = cur << (n-k);
+    whi(cur != lim) {
+        res.push_back(cur);
+        con T lb = cur & -cur, bs = cur + lb;
+        cur = ((bs ^ cur) >> (ctz(lb)+2)) | bs;
+    } res.push_back(cur);
+    ret res;
+}
+
+// 最低位的01变10，其后连续的1移动到低位
+// bs是01变10后高位的状态(除去其后连续的1)
+// bs^cur是操作后受影响的数位状态(1表示反转)
+// 易知popcount(bs^cur)-2即是其后连续的1的个数
+// bs^cur前导零个数与lb相同，因此直接 >> (ctz(lb)+2)可移动连续1至低位
+// cur     lb      bs      bs^cur
+// 010110  000010  011000  001110
+
+// 另附上cout打印二进制的方法
+// cout << bin(v) << endl;
+template<class T=us> using bin = bitset<(sf(T)<<3)>;
