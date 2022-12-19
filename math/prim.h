b9a6f6895d0f77d311b18da5de6a927cb02e4093
lintkey <lintkey@outlook.com>
2022-12-20 01:51:06 +0800

    [增加]添加一系列关于质数的函数： 线性求欧拉函数、莫比乌斯函数`line_phi(N),line_mu(N)`， 质因数分解`pfac(x)`(同时求`phi(x)`、`mu(x)`) 质数判断`is_prim` [更新]将`euler_prim.h`归档至`prim.h`

---
+++ math/prim.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "../base.h"
+
+// 质因数分解
+// <pfac, cnt, phi, mu>
+template<class T>
+tuple<vector<us>, vector<us>, T, is> pfac(T x) {
+    vector<us> pri, cnt;
+    T phi = x; is mu = 1;
+    for(T i=2; i*i<=x; ++i)
+        if(!(x%i)) {
+            pri.push_back(i); cnt.push_back(1);
+            (phi /= i) *= i - 1; mu = -mu;
+            whi(!((x/=i)%i)) (mu = -mu), ++cnt.back();
+        }
+    if(x!=1) {
+        pri.push_back(x); cnt.push_back(1);
+        (phi /= x) *= x - 1; mu = -mu;
+    }
+    ret make_tuple(pri, cnt, phi, mu);
+}
+
+// pir. phi
+tuple<vector<us>, vector<us>> line_phi(us N) {
+    vector<us> pri, phi(N+1, 0);
+    for(us i=2; i<=N; ++i) {
+        if(!phi[i]) {
+            pri.push_back(i);
+            phi[i] = i - 1;
+        }
+        let MX = N / i;
+        for(let &x: pri) {
+            if(x>MX) break;
+            ef(!(i%x)) {
+                phi[i*x] = phi[i] * x;
+                break;
+            } el phi[i*x] = phi[i] * (x-1);
+        }
+    }
+    ret make_tuple(pri, phi);
+}
+
+// pri, phi, mu
+tuple<vector<us>, vector<us>, vector<is>> line_mu(us N) {
+    vector<us> pri, phi(N+1, 0);
+    vector<is> mu(N+1, 0); mu[1] = 1;
+    for(us i=2; i<=N; ++i) {
+        if(!phi[i]) {
+            pri.push_back(i);
+            phi[i] = i + (mu[i]=-1);
+        }
+        let MX = N / i;
+        for(let &x: pri) {
+            if(x>MX) break;
+            ef(!(i%x)) {
+                phi[i*x] = phi[i] * x;
+                break;
+            } el {
+                phi[i*x] = phi[i] * x;
+                mu[i*x] = -mu[i];
+            }
+        }
+    }
+    ret make_tuple(pri, phi, mu);
+}
+
+template<class T>
+bool is_prim(T x) {
+    con us MX = sqrt(x);
+    for(T i=2; i<=MX; ++i)
+        if(x%i) ret 0;
+    ret 1;
+}
