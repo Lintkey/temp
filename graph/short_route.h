967814147493c9c2b84cf041057759c415e16c78
lintkey <lintkey@outlook.com>
2022-12-24 14:13:18 +0800

    [修复]兼容signed

--- graph/short_route.h
+++ graph/short_route.h
@@ -5,7 +5,7 @@
 // struct EI { v; w; }
 template<class G, class D> // G=CFS<MAXN, MAXE, EI> 为了简略使用模板
 inl void dij(G &g, us s, bool *vis, D *dis) {
-    mem(dis, g.n, 0xff);
+    mem(dis, g.n, 0x3f);
     mem(vis, g.n);
     using pdu = pair<D, us>;
     priority_queue<pdu, vector<pdu>, greater<pdu>> que;
@@ -23,7 +23,7 @@ inl void dij(G &g, us s, bool *vis, D *dis) {
 // struct EI { v; w; }
 template<class G, class D>
 inl void spfa(G &g, us s, bool *vis, D *dis) {
-    mem(dis, g.n, 0xff); dis[s]=0;
+    mem(dis, g.n, 0x3f); dis[s]=0;
     mem(vis, g.n); vis[s]=1;
     queue<us> que; que.push(s);
     D ds; typename G::E *ei;
