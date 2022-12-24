f2adc0f528c1042418bd0ad3c346c6715e882dfd
lintkey <lintkey@outlook.com>
2022-12-24 22:12:00 +0800

    [修复]修复init的错误初始化

--- stl/dset.h
+++ stl/dset.h
@@ -2,23 +2,24 @@
 #pragma once
 #include "../base.h"
 
+// WARN: INIT!!!
 template<us LEN>
 struct DSet {
     us far[LEN] = {0}, rnk[LEN] = {0};
-    inl void init(us l, us r) { // WARN: INIT!!!
-        for(; l<r; ++l) far[l] = l;
+    inl void init(us l, us r) {
         mem(rnk+l, 0, r-l);
+        for(; l<r; ++l) far[l] = l;
     }
     us find(us u) {
         if(far[u]==u) ret u;
         else ret far[u] = find(far[u]);
     }
     inl bool same(us l, us r) { ret find(l) == find(r); }
-    // WARN: l'、r'会变为合并后的父、子节点
-    inl bool merge(us &l, us &r) {
+    inl bool merge(us l, us r) {
         if((l=find(l)) == (r=find(r))) ret 0;
         if(rnk[l]<rnk[r]) swap(l, r); // WARN: l',r'与l,r无一一对应关系
-        else if(rnk[l]==rnk[r]) ++rnk[l]; // 按高度度合并
+        ef(rnk[l]==rnk[r]) ++rnk[l];    // 按高度度合并
+        // rnk[l] += rnk[r] + 1;           // 按大小
         ret far[r]=l, 1;
     }
 };
