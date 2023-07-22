db356eb17b174398e112538c1147dace7b01d5d6
lintkey <lintkey@outlook.com>
2023-07-23 00:33:13 +0800

    [更新]`graph.h`更名为`cfs.h`

--- graph/cfs.h
+++ graph/cfs.h
@@ -7,19 +7,10 @@ template<us NLEN, us ELEN=NLEN*2, class EI=us>
 struct CFS {  // 链式前向星
     using E = EI; // 此声明用于辅助其他算法的封装
     us n, hd[NLEN], nx[ELEN], it; EI val[ELEN];
-    void init(us _n) { mem(hd, n=_n, it=-1); } // WARN: INIT!!!
+    void init(us _n) { mes(hd, n=_n, it=-1); } // WARN: INIT!!!
     void add(us u, con EI &ei) {
         nx[++it] = hd[u]; val[it]=ei; hd[u]=it; }
     us head(con us u) con { ret hd[u]; }
     void next(us &e) con { e=nx[e]; }
     EI &operator[](con us e) { ret val[e]; }
 };
-
-// template<class EI>
-// struct CFSBase {
-//     virtual void init(us _n) = 0;
-//     virtual void add(us u, EI ei) = 0;
-//     virtual us head(con us u) con = 0;
-//     virtual void next(us &e) con = 0;
-//     virtual EI &operator[](con us e) = 0;
-// };
