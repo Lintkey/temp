c72f9ed5a5d345e048968ed84666154d54b43d32
lintkey <lintkey@outlook.com>
2023-07-30 22:08:34 +0800

    [修正]删除`inl`

--- stl/pool_ite.h
+++ stl/pool_ite.h
@@ -4,15 +4,15 @@
 // 对反复开辟删除碎片空间可采用此做法
 template<us S=0>
 struct pit {
-    Vec<us> vec;
+    vector<us> vec;
     us ite = S - 1;
-    inl void init() { ite = S - 1; vec.clear(); }
-    inl us nt() {
+    void init() { ite = S - 1; vec.clear(); }
+    us nt() {
         if(vec.empty()) ret ++ite;
-        el { us res = vec.back(); vec.pop_back(); ret res; }
+        else { us res = vec.back(); vec.pop_back(); ret res; }
     }
-    inl void gc(us idx) { vec.push_back(idx); }
-    inl us operator++() { ret nt(); }
-    inl us end() { ret ite + 1; }
-    inl us sz() { ret ite + 1 - S; }
+    void gc(us idx) { vec.push_back(idx); }
+    us operator++() { ret nt(); }
+    us end() { ret ite + 1; }
+    us sz() { ret ite + 1 - S; }
 };
--- stl/treaps.h
+++ stl/treaps.h
@@ -3,32 +3,32 @@
 #include "../base.h"
 #include "pool_ite.h"
 
-template<us LEN, class T, us NUM = 1, us S = 0>
+template<us LEN, class T, us NUM = 1>
 struct Treaps {
     stc cer us SZ = LEN + 1;
-    us rt[S+NUM], ln[SZ], rn[SZ];
+    us rt[NUM], ln[SZ], rn[SZ];
     pit<1> ite;
     us sz[SZ], ct[SZ], lev[SZ];
     T val[SZ];
     Treaps(us N=1) { ln[0] = rn[0] = 0; init(N); }
-    void init(us N=1) { ite.init(); mem(rt, N); }
-    inl void up_siz(us u) { sz[u] = sz[ln[u]] + sz[rn[u]] + ct[u]; }
-    inl void rot_left(us &u) {
+    void init(us N=1) { ite.init(); mes(rt, N); }
+    void up_siz(us u) { sz[u] = sz[ln[u]] + sz[rn[u]] + ct[u]; }
+    void rot_left(us &u) {
         us f = rn[u]; rn[u] = ln[f]; ln[f] = u;
         sz[f] = sz[u]; up_siz(u); u = f;
     }
-    inl void rot_right(us &u) {
+    void rot_right(us &u) {
         us f = ln[u]; ln[u] = rn[f]; rn[f] = u;
         sz[f] = sz[u]; up_siz(u); u = f;
     }
 
-    inl us add(us &u, con T &v) {
+    us add(us &u, con T &v) {
         if(u) {
             ++sz[u];
             if(val[u]==v) ++ct[u];
-            ef(val[u]>v) { if(add(ln[u], v)<lev[u]) rot_right(u); }
-            ef(add(rn[u], v)<lev[u]) rot_left(u);
-        } el {
+            else if(val[u]>v) { if(add(ln[u], v)<lev[u]) rot_right(u); }
+            else if(add(rn[u], v)<lev[u]) rot_left(u);
+        } else {
             u = ++ite; ln[u] = rn[u] = 0;
             sz[u] = ct[u] = 1; lev[u] = rand();
             val[u] = v;
@@ -36,53 +36,53 @@ struct Treaps {
     }
 
     // all指定是否删除全部元素
-    inl void rm(us &u, con T &v, bool all=false) {
+    void rm(us &u, con T &v, bool all=false) {
         stc us _del;
         if(u) {
             if(val[u]==v) {
                 _del = all?ct[u]:1;
                 if(ct[u]>_del) sz[u] -= _del, ct[u] -= _del;
-                el {
+                else {
                     if(ln[u]&&rn[u]) {
                         if(lev[ln[u]]>lev[rn[u]]) rot_left(u), rm(u, v, all);
-                        el rot_right(u), rm(u, v, all);
-                    } el ite.gc(u), u = ln[u] + rn[u];
+                        else rot_right(u), rm(u, v, all);
+                    } else ite.gc(u), u = ln[u] + rn[u];
                 }
-            } el {
+            } else {
                 if(val[u]<v) rm(rn[u], v);
-                el rm(ln[u], v);
+                else rm(ln[u], v);
                 sz[u] -= _del;
             }
-        } el _del = 0;
+        } else _del = 0;
     }
 
-    inl us get_rank(us u, con T &v) {
+    us get_rank(us u, con T &v) {
         us res = 0;
         whi(u) {
             if(val[u]>v) u = ln[u];
-            ef(val[u]==v) ret res + sz[ln[u]];
-            el { res += sz[ln[u]] + ct[u]; u = rn[u]; }
+            else if(val[u]==v) ret res + sz[ln[u]];
+            else { res += sz[ln[u]] + ct[u]; u = rn[u]; }
         } ret res;
     }
 
-    inl con T& rank_find(us u, us k) {
+    con T& rank_find(us u, us k) {
         assert(k<sz[u]);
         whi(u) {
             if(k<sz[ln[u]]) u = ln[u];
-            ef(k>=sz[ln[u]]+ct[u]) {
+            else if(k>=sz[ln[u]]+ct[u]) {
                 k -= sz[ln[u]] + ct[u]; u = rn[u];
-            } el ret val[u];
+            } else ret val[u];
         } exit(1);
     }
 
     // 获取第一个(>v的)值的位置，若v>=所有值返回ite+1
     // WARN: 不能用于修改和查询区间数量
     // 区间数量查询请用get_rank(r)-get_rank(l)
-    inl us upper(us u, con T &v) {
+    us upper(us u, con T &v) {
         us res = end();
         whi(u) {
             if(val[u]<=v) u = rn[u];
-            el { res = u; u = ln[u]; }
+            else { res = u; u = ln[u]; }
         } ret res;
     }
 
@@ -93,8 +93,8 @@ struct Treaps {
         us res = end();
         whi(u) {
             if(val[u]<v) u = rn[u];
-            ef(v<val[u]) { res = u; u = ln[u]; }
-            el ret u;
+            else if(v<val[u]) { res = u; u = ln[u]; }
+            else ret u;
         } ret res;
     }
 
